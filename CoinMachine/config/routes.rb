86a99e858707d9ccd14d4ebfbba54ec61c3733fe
Oliver <oliverzzzzaaa@gmail.com>
2020-03-24 01:32:20 -0700

    updated routes

--- CoinMachine/config/routes.rb
+++ CoinMachine/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do 
+    # I created them under the API namespace since these are API routes
+    resources :users, only: [:create]
+    resources :coins
+    # Coincount to count the total value of all coins
+    get '/coincount', to: 'coins#count'
+    resources :transactions, only: [:index, :create]
+  end
 end
