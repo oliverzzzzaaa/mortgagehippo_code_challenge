95be97dfe94f70ad32930720e1ada59976dcd6dc
Oliver <oliverzzzzaaa@gmail.com>
2020-03-24 01:36:47 -0700

    added require api key

--- CoinMachine/app/controllers/application_controller.rb
+++ CoinMachine/app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_method :require_api_key
+    skip_before_action :verify_authenticity_token
+
+    # Skip before action required for API keys, to avoid authenticity token from rails
+    # Require api key for all actions, except creating a new user 
+
+    def require_api_key
+        @user = User.find_by(api_key: params[:api_key])
+        unless @user
+            # Will render this if an invalid API key is provided
+            render json: ['Invalid API key']
+        end
+    end
+
 end
